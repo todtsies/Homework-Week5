b7856cb0668846f0d1b8b890b0c08b12c95103f8
todtsies <emilybtodt@gmail.com>
2020-12-15 17:03:15 -0500

    script file didn't work
    The time was the only thing I could get to work so I'm restarting but leaving the current time in the header

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-//Setting my variables
-var hourNine = $("#9");
-var hourTen = $("#10");
-var hourEleven = $("#11");
-var hourTwelve = $("#12")
-var hourOne = $("#1");
-var hourTwo = $("#2");
-var hourThree = $("#3");
-var hourFour = $("#4");
-var hourFive = $("#5");
+
 var time = moment();
 
+function setPlanner() {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+}
+
+setPlanner();
