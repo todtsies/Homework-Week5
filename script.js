ad9c9f402c229f17c08364f327af4d864af047bd
todtsies <emilybtodt@gmail.com>
2020-12-15 16:24:25 -0500

    Update in index.html and start js file
    Added new ID to rows and set my variables in my Javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+//Setting my variables
+var hourNine = $("#9");
+var hourTen = $("#10");
+var hourEleven = $("#11");
+var hourTwelve = $("#12")
+var hourOne = $("#1");
+var hourTwo = $("#2");
+var hourThree = $("#3");
+var hourFour = $("#4");
+var hourFive = $("#5");
+var time = moment();
+
